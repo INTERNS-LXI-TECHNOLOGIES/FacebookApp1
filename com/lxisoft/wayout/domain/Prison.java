473d6424720ca8658a9639c42ec5a63b6c1192ce
aparna-udayakumar <aparna.u@lxisoft.com>
2018-07-28 09:51:54 +0530

    prison modified LI-298

--- com/lxisoft/wayout/domain/Prison.java
+++ com/lxisoft/wayout/domain/Prison.java
@@ -1,7 +1,8 @@
 package com.lxisoft.wayout.domain;
 
 /**
-*Setting and getting of  variable hall in class Prison.
+*Setting and getting of  variables prisoner and hall in class Prison.
+*@author Aparna.
 *@author Manoj.
 */
 
@@ -9,14 +10,34 @@ public class Prison
 { 
 	
 /**
-*Declaration of variable  hall of type Hall.
+*Declaration of variable prisoner of type Prisoner.
 */
-    
+    private Prisoner prisoner;
+/**
+*Declaration of variable and hall of type Hall.
+*/
 	private Hall hall;
+/**
+*Setting prisoner.
+*@param prisoner (Passing parameter prisoner).
+*/
+    public void setPrisoner(Prisoner prisoner)
+    {
+    this.prisoner=prisoner;
+	}
+/**
+*Getting prisoner.
+*@return prisoner (Returning the value prisoner).
+*/
+	
+	public Prisoner getPrisoner()
+	{
 
+		return this.prisoner;
+	}
 /**
 *Setting hall.
-*@param hall Passing parameter hall.
+*@param hall (Passing parameter hall).
 */
 	public void setHall(Hall hall)
 	{
@@ -24,7 +45,7 @@ public class Prison
 	}
 /**
 *Getting hall.
-*@return hall Returning the value hall.
+*@return hall (Returning the value hall).
 */
 	public Hall getHall()
 	{
