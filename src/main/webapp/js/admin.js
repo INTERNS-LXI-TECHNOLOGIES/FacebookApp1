45a4aa6b75dd886b334907ae9d4a0681428de1ac
sanilkumarp <sanilkumar.p@lxisoft.com>
2018-08-08 16:30:51 +0530

    LI-514

--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -7,6 +7,15 @@ function displayOptions(){
 	for (i = 1; i <= j; i++) { 
 		var option="option"+i;
     	document.getElementById(option).style.display='block';
-    	console.log(document.getElementById(option));
-}
+    	document.getElementById(option).required=true;
+    	//console.log(document.getElementById(option));
+    	console.log("i="+i+document.getElementById(option).required);
+	}
+	for (j=i; j <= 8; j++) { 
+		var option="option"+j;
+    	document.getElementById(option).style.display='none';
+    	document.getElementById(option).required=false;
+    	//console.log(document.getElementById(option));
+    	console.log("j="+j+document.getElementById(option).required);
+	}
 }
